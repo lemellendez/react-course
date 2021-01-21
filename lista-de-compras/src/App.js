47bcf7559781bd19ccb3060a68fb2a000ec9497d
Leticia Melendez <leticiamelendez@gmail.com>
2021-01-21 00:37:01 -0300

    Exercicio Lista de Compras

---
+++ lista-de-compras/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import ListaDeCompras from './ListaDeCompras'
+
+function App() {
+  return ListaDeCompras;
+}
+
+export default App;
---
+++ lista-de-compras/src/ListaDeCompras.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Item from './Item'
+
+function ListaDeCompras() {
+  return (
+    <div>
+      <Item nome="Arroz" />
+      <Item nome="Vinho Branco" />
+      <Item nome="Macarrão" />
+      <Item nome="Creme de Leite" />
+      <Item nome="Ossobuco" />
+    </div>
+  );
+}
+
+export default ListaDeCompras;
